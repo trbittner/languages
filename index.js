a0e9b420b3f6c94571a429561c4b54d8cf53892c
Todd Bittner <toddbittner@gmail.com>
2023-07-09 12:29:30 +0000

    Created first attempt at scraping.

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 import axios from 'axios';
+import cheerio from 'cheerio';
 
 const url = 'https://simple.wikipedia.org/wiki/Wikipedia:Basic_English_combined_wordlist';
 
 axios(url)
     .then(response => {
         const html = response.data;
-        console.log(html);
+        const $ = cheerio.load(html);
+        const basics = createWordArray($,'Basic')
+        const international = createWordArray($,'International')
+        const addendum = createWordArray($,'Addendum')
+        const compound = createWordArray($,'Compound')
+        const endings = createWordArray($,'Endings')
+        const words = [...basics,...international,...addendum,...compound,...endings];
+
+        console.log(words.length)
+        
+        for (let word of words) {
+            console.log(word);
+        }
+        
+        //console.log(basics)
+        
     })
     .catch(console.error);
+    
+const createWordArray = (domParser,key) => {
+    return domParser('b:contains("' + key + ':")').nextAll('a').
+        map((index,element) => domParser(element).text()).get();
+}
