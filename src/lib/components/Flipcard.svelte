61c95be1c94f00cb0f3dda0de37a5792cb937591
Todd Bittner <toddbittner@gmail.com>
2023-07-20 18:45:48 +0000

    Created a basic working flipcard.

--- src/lib/components/Flipcard.svelte
+++ src/lib/components/Flipcard.svelte
@@ -3,12 +3,19 @@
     question: "What is the capital of France?",
     answer: "Paris"
   };
+  
+  let isFlipped = false;
+  
+  const flipCard = () => {
+      isFlipped = !isFlipped;
+  }
 </script>
 
 <div class="flex items-center justify-center h-screen">
-  <div class="w-1/2 aspect-video bg-blue-300 rounded-md shadow-md flex items-center justify-center flip-card">
-    <div class="flip-card-inner relative w-full h-full">
-      <div class="flip-card-front flex items-center justify-center absolute w-full h-full z-10">
+  <div class="w-1/2 aspect-video bg-blue-300 rounded-md shadow-md flex items-center justify-center flip-card" 
+    on:click={flipCard} class:bg-yellow-200={isFlipped} >
+    <div class="flip-card-inner relative w-full h-full" class:flipped={isFlipped}>
+      <div class="flip-card-front flex items-center justify-center absolute w-full h-full">
         <p class="text-lg text-gray-700 text-center">{cardContent.question}</p>
       </div>
       <div class="flip-card-back flex items-center justify-center absolute w-full h-full">
@@ -26,6 +33,10 @@
   .flip-card-inner {
     transition: transform 0.6s;
     transform-style: preserve-3d;
+  }
+  
+  .flipped {
+    transform: rotateY(180deg);
   }
   
   .flip-card-front,
