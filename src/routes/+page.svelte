a46f2705a9a78bb84fc122aee1ebe7c83fe4bb4d
Todd Bittner <toddbittner@gmail.com>
2023-07-21 02:01:00 +0000

    Added arrows for traversing content to the main page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
     import Flipcard from '$lib/components/Flipcard.svelte'
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<Flipcard />
+<div class="flex flex-col items-center justify-center h-screen">
+    <Flipcard />
+    <div class="mt-3 flex justify-evenly w-full lg:w-1/2">
+        <button class="bg-yellow-400 basis-1/5 rounded-md"><span class="text-2xl">&#8592;</span></button>
+        <button class="bg-yellow-400 basis-1/5 rounded-md"><span class="text-2xl">&#8594;</span></button>
+    </div>
+</div>
